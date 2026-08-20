d31ed83a8203c16e73ebebe6f969acd768e21bda
elsouza481-creator <elsouza.481@gmail.com>
2026-08-20 13:57:33 -0300

    test: register free roam combat authority

--- core/src/test/kotlin/grandlineduo/test/TestRunner.kt
+++ core/src/test/kotlin/grandlineduo/test/TestRunner.kt
@@ -109,6 +109,7 @@ fun main() {
     grandlineduo.game.world.ExplorationLootEngineTest.register()
     grandlineduo.game.world.ExplorationLootCommandIntegrationTest.register()
     grandlineduo.game.world.ExplorationCombatEngineTest.register()
+    grandlineduo.game.world.ExplorationCombatCommandIntegrationTest.register()
 
     var failed = 0
     for (case in TestRegistry.all()) {
