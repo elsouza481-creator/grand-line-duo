7fb1f371afbbbdd76589a4634fccbed960fe623d
elsouza481-creator <elsouza.481@gmail.com>
2026-08-20 16:02:02 -0300

    test: register class training rules suite

--- core/src/test/kotlin/grandlineduo/test/TestRunner.kt
+++ core/src/test/kotlin/grandlineduo/test/TestRunner.kt
@@ -71,6 +71,7 @@ fun main() {
     grandlineduo.game.character.ClassMasteryEngineTest.register()
     grandlineduo.game.character.ClassMasteryPersistenceTest.register()
     grandlineduo.game.character.ClassMasteryTrainingIntegrationTest.register()
+    grandlineduo.game.character.ClassTrainingRulesTest.register()
     grandlineduo.game.character.CharacterCoopIntegrationTest.register()
     grandlineduo.game.powers.HakiEngineTest.register()
     grandlineduo.game.powers.DevilFruitEngineTest.register()
