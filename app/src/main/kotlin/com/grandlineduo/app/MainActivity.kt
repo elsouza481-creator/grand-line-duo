6dd0d9dd6483e23bf2d164e272c48f95634397f1
elsouza481-creator <elsouza.481@gmail.com>
2026-08-20 17:43:28 -0300

    feat: dispatch training duel actions on Android

--- app/src/main/kotlin/com/grandlineduo/app/MainActivity.kt
+++ app/src/main/kotlin/com/grandlineduo/app/MainActivity.kt
@@ -162,7 +162,8 @@ class MainActivity : Activity() {
                     "POWER" -> coordinator.submitPowerAction(action.id)
                     "VOYAGE" -> coordinator.submitVoyageAction(VoyageAction.valueOf(action.id))
                     "EXPLORE_MOVE" -> coordinator.submitWorldAction("EXPLORE_MOVE", action.id, 1)
-                    "QUEST_ACCEPT", "QUEST_PROGRESS", "QUEST_TURN_IN", "LOOT_COLLECT" ->
+                    "QUEST_ACCEPT", "QUEST_PROGRESS", "QUEST_TURN_IN", "LOOT_COLLECT",
+                    "DUEL_CHALLENGE", "DUEL_ACCEPT", "DUEL_DECLINE", "DUEL_ACTION", "DUEL_FORFEIT" ->
                         coordinator.submitWorldAction(action.kind, action.id, 1)
                     "CAMPAIGN" -> coordinator.advanceCampaign(action.id)
                     else -> throw IllegalArgumentException("Ação não suportada: ${action.kind}")
