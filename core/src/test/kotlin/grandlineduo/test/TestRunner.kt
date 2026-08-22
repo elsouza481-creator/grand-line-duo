4a34cfaab765e7fb93f9c7a7fb5f55856577ccf6
elsouza481-creator <elsouza.481@gmail.com>
2026-08-22 19:54:41 -0300

    test: register hunt handler routing coverage

--- core/src/test/kotlin/grandlineduo/test/TestRunner.kt
+++ core/src/test/kotlin/grandlineduo/test/TestRunner.kt
@@ -50,6 +50,7 @@ fun main() {
     grandlineduo.game.quest.QuestObjectiveRouterTest.register()
     grandlineduo.game.quest.QuestHuntFactoryTest.register()
     grandlineduo.game.quest.QuestHuntCoordinatorTest.register()
+    grandlineduo.game.quest.QuestHuntRoutingTest.register()
     grandlineduo.game.quest.QuestDirectorBridgeTest.register()
     grandlineduo.game.quest.QuestPersistenceTest.register()
     grandlineduo.game.quest.QuestLanIntegrationTest.register()
