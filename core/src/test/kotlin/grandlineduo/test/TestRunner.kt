8c56b9dcc36e1d68e23825bf448f4c26a0d90dde
elsouza481-creator <elsouza.481@gmail.com>
2026-08-22 19:45:04 -0300

    test: register hunt factory coverage

--- core/src/test/kotlin/grandlineduo/test/TestRunner.kt
+++ core/src/test/kotlin/grandlineduo/test/TestRunner.kt
@@ -48,6 +48,7 @@ fun main() {
     grandlineduo.game.director.GrandLineDirectorTest.register()
     grandlineduo.game.quest.QuestEngineTest.register()
     grandlineduo.game.quest.QuestObjectiveRouterTest.register()
+    grandlineduo.game.quest.QuestHuntFactoryTest.register()
     grandlineduo.game.quest.QuestDirectorBridgeTest.register()
     grandlineduo.game.quest.QuestPersistenceTest.register()
     grandlineduo.game.quest.QuestLanIntegrationTest.register()
