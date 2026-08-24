a19929ace14be21e0a8a780e8b76be39cda3eb65
elsouza481-creator <elsouza.481@gmail.com>
2026-08-24 09:24:07 -0300

    test: register field quest tcp integration

--- core/src/test/kotlin/grandlineduo/test/TestRunner.kt
+++ core/src/test/kotlin/grandlineduo/test/TestRunner.kt
@@ -55,6 +55,7 @@ fun main() {
     grandlineduo.game.quest.QuestHuntRoutingTest.register()
     grandlineduo.game.quest.QuestFieldRoutingTest.register()
     grandlineduo.game.quest.QuestHuntLanIntegrationTest.register()
+    grandlineduo.game.quest.QuestFieldLanIntegrationTest.register()
     grandlineduo.game.quest.QuestDirectorBridgeTest.register()
     grandlineduo.game.quest.QuestPersistenceTest.register()
     grandlineduo.game.quest.QuestLanIntegrationTest.register()
